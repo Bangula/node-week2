849d68829de40b95f4492d1893dca13038966b64
Branko Vukovic <branko.vukovic@quantox.com>
2021-06-20 18:25:55 +0200

    http client exercise

--- exercises/http-client.js
+++ exercises/http-client.js
@@ -1,12 +1,8 @@
-asdflkasjdf
-asdfasdfsadf
-asdfasdfasdf
-asdfasdfasdf
-
 const http = require("http");
 const process = require("process");
 
 http.get(process.argv[2], (response) => {
+  response.setEncoding("utf8");
   response.on("data", (data) => {
     console.log(data);
   });
